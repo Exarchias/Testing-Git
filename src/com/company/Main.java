21961e6a271cf4f48cbd148ee2fc57a6c9056816
Nzams25 <nzamsguillaum@yahoo.com>
2019-12-13 00:27:11 +0100

    add method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        add(2,3);
+    }
+    public static void add(int a, int b){
+        System.out.println("adding integers");
+
     }
 }
